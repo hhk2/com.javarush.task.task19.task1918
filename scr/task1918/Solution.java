d3d46ceed7e414c9df3c5e901f586cc3b6f2582f
hhk2 <hhk2@inbox.ru>
2018-03-05 22:53:31 +0300

    добавил комменты

--- scr/task1918/Solution.java
+++ scr/task1918/Solution.java
@@ -14,33 +14,34 @@ public class Solution {
         BufferedReader bufferedReader = new BufferedReader(new FileReader(reader.readLine()));
         StringBuilder in = new StringBuilder();
         while (bufferedReader.ready()) {
-            in.append(bufferedReader.readLine());
+            in.append(bufferedReader.readLine());//Читаем строки в StringBuilder
         }
         reader.close();
         bufferedReader.close();
         if (in.indexOf("<") != 0) {
-            in.delete(0, in.indexOf("<"));
+            in.delete(0, in.indexOf("<"));//удоляем лишнее в начале файла
         }
         String st = in.toString();
         if (!st.equals("")) {
-            Pattern outpatternStart = Pattern.compile("<" + args[0] + "(.*?)>");
-            Pattern outpatternEnd = Pattern.compile("</" + args[0] + ">");
+            Pattern outpatternStart = Pattern.compile("<" + args[0] + "(.*?)>");//Pattern для поиска начала тега
+            Pattern outpatternEnd = Pattern.compile("</" + args[0] + ">");//Pattern для поиска конца тега
             Matcher outMstcherStart = outpatternStart.matcher(st);
             Matcher outMstcherEnd = outpatternEnd.matcher(st);
-            ArrayList<Integer> StarinexsStart = new ArrayList<>();
-            ArrayList<Integer> EndinexsEnd = new ArrayList<>();
-            ArrayList<Integer> StarinexsEnd = new ArrayList<>();
-            ArrayList<Integer> EndinexsStart = new ArrayList<>();
+            ArrayList<Integer> StarinexsStart = new ArrayList<>();//лист начадьых индексов начал тегов
+            ArrayList<Integer> EndinexsEnd = new ArrayList<>();//лист конечных индексов начал тегов
+            ArrayList<Integer> StarinexsEnd = new ArrayList<>();//лист начадьых индексов концов тегов
+            ArrayList<Integer> EndinexsStart = new ArrayList<>();//лист конечных индексов концов тегов
             while (outMstcherStart.find()) {
-                StarinexsStart.add(outMstcherStart.start());
-                StarinexsEnd.add(outMstcherStart.end());
+                StarinexsStart.add(outMstcherStart.start());//заполнение листа начадьых индексов начал тегов
+                StarinexsEnd.add(outMstcherStart.end());//заполнение листа конечных индексов начал тегов
             }
             while (outMstcherEnd.find()) {
-                EndinexsStart.add(outMstcherEnd.start());
-                EndinexsEnd.add(outMstcherEnd.end());
+                EndinexsStart.add(outMstcherEnd.start());//заполнение листа начадьых индексов концов тегов
+                EndinexsEnd.add(outMstcherEnd.end());//запонение листа конечных индексов концов тегов
             }
             int indexMax = (StarinexsEnd.size() + StarinexsStart.size() + EndinexsEnd.size() + EndinexsStart.size()) / 4;
-            for (int i = 0; i < indexMax; i++) {
+            //вычисление сриндегс размера листов
+            for (int i = 0; i < indexMax; i++) { //поиск внутриннихтегов и вывод на эран
                 {
                     Matcher inMattcher = outpatternStart.matcher(st.substring(StarinexsEnd.get(i), EndinexsStart.get(i)));
                     int countgrup = 0;
